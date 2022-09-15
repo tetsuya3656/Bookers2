e1ac9116f7689551f678b6e2541179016ddfd8d3
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-09-15 13:04:32 +0000

    [Delete] Bookコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
+  
   root to: "homes#top"
   devise_for :users
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/homes/about', to: 'homes#about', as: "about"
 
-  resources :books, only: [:new, :index, :show, :create]
+ 
+  resources :users, only: [:show, :edit]
 end
