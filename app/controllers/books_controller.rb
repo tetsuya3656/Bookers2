5125cf1874bcb8cbe1adda62815955af29a381ed
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-09-26 13:54:53 +0000

    [Create] 投稿編集ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,37 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
-    
+
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_bath
-  end  
+    redirect_to book_bath(@book.id)
+  end
 
   def index
+    @book = Book.find(params[:id])
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(@book.id)
+  end 
   
   private
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
-  end 
-  
+  end
+
 end
